775642a227db9b90ff6927ca7a23b1bb3fe7a009
Thidtanai <thidtanai_k@cmu.ac.th>
2021-01-15 15:31:51 +0700

    lab11-1 complete

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int input);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int input){
+  if(input <= 1)return input;
+  else return fibonacci(input-1) + fibonacci(input-2);
 }
