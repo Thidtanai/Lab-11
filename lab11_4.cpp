f4cc25dbd2cce185945e796e96438e54e112c39b
Thidtanai <thidtanai_k@cmu.ac.th>
2021-01-15 17:48:11 +0700

    complete

--- lab11_4.cpp
+++ lab11_4.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
-
 using namespace std;
+
+template <class T>
+void mySwap(T &,T &);
 
 int main(){
 	int x, y;
@@ -30,3 +32,8 @@ int main(){
 	return 0;
 }
 
+template <class T>
+void mySwap(T &x,T &y){
+  T a =x , b =y;
+  x = b, y = a;
+}
